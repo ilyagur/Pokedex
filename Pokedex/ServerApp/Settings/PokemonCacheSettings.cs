589b035aa975f850a071aefd4006ec09a7dfbdbe
Ilya Gurzhiy <ilya.gurzhiy@gmail.com>
2017-12-02 11:02:27 +0300

    Remove old Pokedex

--- Pokedex/ServerApp/Settings/PokemonCacheSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Pokedex.ServerApp.Settings {
-    public class PokemonCacheSettings {
-
-        public string PokemonCacheFolder { get; set; }
-        public string PokemonListCacheKey { get; set; }
-    }
-}
--- Pokedex/ServerApp/Settings/PokemonHttpClientAdapterSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Pokedex.ServerApp.Settings {
-    public class PokemonHttpClientAdapterSettings
-    {
-        public string BaseApiUrl { get; set; }
-        public string PokemonListEndpoint { get; set; }
-        public string PokemonListOptions { get; set; }
-    }
-}
